216501811d2d87dbb4845217a43eba2701382417
sadiejaneb <sadiejaneb@gmail.com>
2023-11-18 18:56:09 -0500

    blocking halves player damage

--- Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -6,19 +6,28 @@ public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     private int currentHealth;
+    private Animator animator;
 
     void Start()
     {
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
     }
 
     public void ApplyDamage(int damage)
     {
-        currentHealth -= damage;
+        // Check if the player is blocking
+        bool isBlocking = animator.GetBool("IsBlocking");
+
+        // Halve the damage if blocking
+        int damageTaken = isBlocking ? damage / 2 : damage;
+
+        currentHealth -= damageTaken;
         Debug.Log("Player health: " + currentHealth);
+
         if (currentHealth <= 0)
         {
-            // Handle character death
+            animator.SetTrigger("IsDead"); // Trigger the death animation
         }
     }
 }
